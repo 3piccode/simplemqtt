d8bc84575362ea91f0bf641f132a074c4e59e7bf
Wekhrozo Medo <khroz.meds@gmail.com>
2017-06-17 17:18:32 +0530

    Update simplemqtt.h

--- simplemqtt.h
+++ simplemqtt.h
@@ -3,21 +3,21 @@
 #ifndef simplemqtt_H
 #define simplemqtt_H
 
-void initMQTT_cli(const char *domain_host, uint16_t port, PubSubClient cli, const char *user, const char *pwd, const char *cli_ID);
-void initMQTT_cli_callBack();
+void initSimplemqtt(const char *domain_host, uint16_t port, PubSubClient cli, const char *user, const char *pwd, const char *cli_ID);
+void initSimplemqtt_callBack();
 
-void MQTT_cli_ReConnect(const char *subTopic);
+void Simplemqtt_ReConnect(const char *subTopic);
 
-bool MQTT_cli_subscribe(const char *topic, uint8_t qos);
+bool Simplemqtt_subscribe(const char *topic, uint8_t qos);
 
-bool MQTT_cli_unSubscribe(const char* topic);
+bool Simplemqtt_unSubscribe(const char* topic);
 
-String MQTT_cli_callBack_Subscribe_Msg_String();
+String Simplemqtt_callBack_Subscribe_Msg_String();
 
-bool MQTT_cli_publish(const char* topic, const char* payload, bool retained);
+bool Simplemqtt_publish(const char* topic, const char* payload, bool retained);
 
-void MQTT_cli_loop();
+void Simplemqtt_loop();
 
-void MQTT_cli_disconnect();	
+void Simplemqtt_disconnect();	
 
 #endif
